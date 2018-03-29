74be46aff929423a5bb05e05f312a3edec7f0f88
Thomas Viehmann <tv.code@beamnet.de>
2018-03-29 08:08:26 +0200

    first backward for bilinear

--- aten/src/ATen/native/Linear.cpp
+++ aten/src/ATen/native/Linear.cpp
@@ -45,4 +45,43 @@ Tensor bilinear(const Tensor& input1, const Tensor& input2, const Tensor& weight
   return output;
 }
 
+std::tuple<Tensor, Tensor, Tensor, Tensor> bilinear_backward(const Tensor& grad_out, const Tensor& input1, const Tensor& input2,
+							     const Tensor& weight, const Tensor& bias, std::array<bool, 4> grad_mask) {
+  Tensor grad_input1, grad_input2, grad_weight, grad_bias;
+
+  size_t output_features = weight.size(0);
+  auto input1_flattened = input1.view({-1, input1.size(-1)});
+  auto input2_flattened = input2.view({-1, input2.size(-1)});
+  auto grad_out_flattened = grad_out.view({-1, grad_out.size(-1)});
+
+  if (grad_mask[0] || grad_mask[1]) {
+    grad_input1 = at::mm(input2_flattened, weight[0].t());
+    grad_input1.mul_(grad_out_flattened.narrow(1, 0, 1));
+    grad_input2 = at::mm(input1_flattened, weight[0]);
+    grad_input2.mul_(grad_out_flattened.narrow(1, 0, 1));
+    for (size_t k = 1; k < output_features; k++) {
+       auto buf = input2_flattened.mm(weight[k].t());
+       buf.mul_(grad_out_flattened.narrow(1, k, 1));
+       grad_input1 += buf;
+       buf = input1_flattened.mm(weight[k]);
+       buf.mul_(grad_out_flattened.narrow(1, k, 1));
+       grad_input2.add_(buf);
+    }
+    grad_input1 = grad_input1.view_as(input1);
+    grad_input2 = grad_input2.view_as(input2);
+  }
+  if (grad_mask[2]) {
+    grad_weight = weight.type().tensor(weight.sizes());
+    for (size_t k = 0; k < output_features; k++) {
+      auto buf = input1_flattened.mul(grad_out_flattened.narrow(1, k, 1));
+      auto weight_row = grad_weight[k];
+      at::mm_out(weight_row, buf.t(), input2_flattened);
+    }
+  }
+  if (grad_mask[3]) {
+    grad_bias = grad_out_flattened.sum(0, false);
+  }
+  return std::tuple<Tensor, Tensor, Tensor, Tensor>(grad_input1, grad_input2, grad_weight, grad_bias);
+}
+
 }}  // namespace at::native
